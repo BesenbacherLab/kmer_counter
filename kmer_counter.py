db63894f82239cf35f6a16404b6b13e8bf7ef7a4
Soeren Besenbacher besen@birc.au.dk <besenbacher@clin.au.dk>
2021-04-15 01:47:41 +0200

    bug fix

--- kmer_counter.py
+++ kmer_counter.py
@@ -10,7 +10,7 @@ def reverse_complement(s):
     return s.translate(complement)[::-1]
 
 WrongNumberOfInputException = \
-    Exception('Exactly one of the input options --wig, --bed, --pos or --all_autosomes should be used')
+    Exception('Exactly one of the input options --bed, --pos or --all_autosomes should be used')
 
 class PosReader:
     def __init__(self, f, tb):
@@ -64,28 +64,53 @@ def get_indel_contexts(chrom, pos, radius, tb):
 
 
 def get_possible_indel_pos(chrom, pos, ref, alt, tb, buffer=50):
+    #print(ref, alt)
     if buffer <= len(alt)+len(ref):
         return get_possible_indel_pos(chrom, pos, ref, alt, tb, buffer*2)
+    # This method assumes that the indel is left aligned
+    # long_seq og short_seq starts at the first position not shared between
+    # alt og ref
     if len(ref) > len(alt): # This variant is a deletion
         assert(len(alt)==1)
         assert(ref[0] == alt)
-        long_seq = tb.sequence(chrom, pos, pos+buffer)
+        #print(tb.sequence(chrom, pos-1, pos+len(ref)-1))
+        long_seq = tb.sequence(chrom, pos, pos+buffer) # Reference sequence
+        #print(long_seq[:len(ref)-1], ref[1:])
+        assert(long_seq[:len(ref)-1] == ref[1:])
         #long_seq = genome[pos:pos+buffer] # Reference sequence
-        short_seq = long_seq[:1] + long_seq[len(ref):] # Alternative sequence
-        i= 1
+        #short_seq = long_seq[:1] + long_seq[len(ref):] # Alternative sequence
+        short_seq = long_seq[len(ref)-1:] # Alternative sequence
+        i= 0
     elif len(ref) < len(alt): # This variant is an Insertion
         assert(len(ref)==1)
+        assert(ref == alt[0])
         #alternative = genome[pos-1:pos] + alt + genome[pos+1:pos+buffer]
         short_seq = tb.sequence(chrom, pos, pos+buffer)
         #short_seq = genome[pos:pos+buffer] # Reference seqeunce
-        long_seq =  alt + short_seq[1:] # Alternative seqeunce
-        #long_seq =  alt + genome[pos+1:pos+buffer] # Alternative seqeunce
+        long_seq =  alt[1:] + short_seq # Alternative seqeunce
+        #long_seq = alt + genome[pos+1:pos+buffer] # Alternative seqeunce
         i = 0 
     else:
         assert(False)
     L = []
     len_diff = len(long_seq) - len(short_seq)
-    #print(short_seq, long_seq)
+    # print(len_diff, len(alt))
+    # print(i)
+    # print("short long")
+    # print(short_seq)
+    # print(long_seq)
+    # print("short start long start")
+    # print(short_seq[:i])
+    # print(long_seq[:i])
+    # if not (short_seq[:i] == long_seq[:i]):
+    #     print('PROBLEM start')
+    #     return [(i+pos, long_seq[i:i+len_diff])]
+    # print("short end long end")
+    # print(short_seq[i:])
+    # print(long_seq[i+len_diff:])
+    # if not (short_seq[i:] == long_seq[i+len_diff:]):
+    #     print('PROBLEM end')
+    #     return [(i+pos, long_seq[i:i+len_diff])]
     assert(short_seq[:i] == long_seq[:i] and short_seq[i:] == long_seq[i+len_diff:])
     while True:     
         #print("prefixes = ", short_seq[:i], long_seq[:i])
@@ -100,7 +125,8 @@ def get_possible_indel_pos(chrom, pos, ref, alt, tb, buffer=50):
             return L
 
 def test_indels():
-    tb = py2bit.open('/Users/sobe/Data/2bit/hg38.2bit')
+    tb = py2bit.open('/home/besen/Data/2bit/hg38.2bit')
+    #tb = py2bit.open('/Users/sobe/Data/2bit/hg38.2bit')
     #seq ='CTCGCGCGTT'
     #for i in range(1000000, 400700100):
     #    if i % 1000000 == 0:
@@ -108,24 +134,24 @@ def test_indels():
     #    if seq == tb.sequence("chr1", i, i+len(seq)):
     #        print(i, seq)
 
-    print(get_possible_indel_pos("chr1", 1012306, "C", "CTC", tb ,1))
+    print(get_possible_indel_pos("chr1", 1012306, "C", "CCT", tb ,1))
     # [(1012306, 'CT'), (1012307, 'TC'), (1012308, 'CT')]
     kmers = []
-    for rpos, ralt in get_possible_indel_pos("chr1", 1012306, "C", "CTC", tb ,1): 
+    for rpos, ralt in get_possible_indel_pos("chr1", 1012306, "C", "CCT", tb ,1): 
         c1, c2 = get_indel_contexts("chr1", rpos, 2, tb)
         kmers.append(c1)
     # ['ACCT', 'CCTT', 'CTTA']
     print(kmers)
     kmers=[]
     # [(40884143, 'CG'), (40884144, 'GC'), (40884145, 'CG'), (40884146, 'GC'), (40884147, 'CG')]
-    print(get_possible_indel_pos("chr1", 40884142, "TCG", "T", tb, 1))
-    for rpos, ralt in get_possible_indel_pos("chr1", 40884142, "TCG", "T", tb, 1): 
+    print(get_possible_indel_pos("chr1", 40884143, "TCG", "T", tb, 1))
+    for rpos, ralt in get_possible_indel_pos("chr1", 40884143, "TCG", "T", tb, 1): 
         c1, c2 = get_indel_contexts("chr1", rpos, 2, tb)
         kmers.append(c1)
     print(kmers)
     #['CTCG', 'TCGC', 'CGCG', 'GCGC', 'CGCG']
     kmers=[]
-    for rpos, ralt in get_possible_indel_pos("chr1", 40884142, "TCG", "T", tb, 1): 
+    for rpos, ralt in get_possible_indel_pos("chr1", 40884143, "TCG", "T", tb, 1): 
         c1, c2 = get_indel_contexts("chr1",rpos+(len("TCG")-len("T")), 2, tb)
         kmers.append(c1)
     print(kmers)
@@ -140,14 +166,18 @@ def count_indels(args):
 
     for line in args.mutations:
         chrom, pos, ref, alt = line.split()[:4]
+        if args.verbose:
+            print(chrom, pos, ref, alt)#, file=sys.stderr)
         pos = int(pos)
         if len(ref) > len(alt):
             if len(alt) != 1:
                 if (ref[-(len(alt)-1):] != alt[1:]):
                     print("Warning. Complex variant ignored:", line.strip(), file=sys.stderr)
                     continue
+                #print("Variant before", alt, ref)
                 ref = ref[:-(len(alt)-1)]
                 alt = alt[:1]
+                #print("Variant after", alt, ref)
         elif len(alt)>len(ref):
             if len(ref) != 1:
                 if(ref[1:] != alt[-(len(ref)-1):]):
@@ -155,7 +185,9 @@ def count_indels(args):
                     continue
                 alt = alt[:-(len(ref)-1)]
                 ref = ref[:1]
-
+        if alt[0] != ref[0]:
+            print("Warning. Not left-aligned variant ignored:", line.strip())
+            continue
         L = get_possible_indel_pos(chrom, pos, ref, alt, tb)
         if args.sample:
             L = [random.choice(L)]
@@ -231,7 +263,8 @@ if __name__ == "__main__":
     Count k-mers at SNVs, indel breakpoints or in a background file
     ''')
     subparsers = parser.add_subparsers(dest='command', help='command. Must choose what kind of file you want to count.')
-    #parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-t', '--test', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
     #parser.add_argument('-l', '--log_file', type=argparse.FileType('w'))
     
     snv_parser = subparsers.add_parser('snv', description='Count k-mers at SNVs.')
@@ -256,6 +289,8 @@ if __name__ == "__main__":
         help='How many base pairs before indel_start_point or after indel_end_point should be included '
         'as context annotation.')
     indel_parser.add_argument('--sample', action="store_true", help='Randomly choose one of the possible positions instead of counting the expected (non integer) count for each possible position of an ambigously aligned indel.')
+    indel_parser.add_argument('-v', '--verbose', action='store_true')
+
 
     bg_parser = subparsers.add_parser('background', description='Count kmers in a genome')
     bg_parser.add_argument('ref_genome',  type=str,
@@ -279,6 +314,9 @@ if __name__ == "__main__":
         help='"none" means that alle k-mers are counted unchanged. "middle" means that the reverse complement of a k-mer is counted if the middle position is not a "A" or "C". "lexicographic" means that the reverse_complement is counted if it has a smaller lexicographic order. Default is "middle" if --radius option is used and "lexicographic" if --before_after is used.')
 
     args = parser.parse_args()
+    if args.test:
+        test_indels()
+        sys.exit()
 
     tb = py2bit.open(args.ref_genome)
 
